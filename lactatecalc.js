2e6309e8ffa2c77c4e80a82e927831d05ae3e8fd
bishoplab <dale.taylor@vu.edu.au>
2025-02-06 16:28:17 +1100

    Update lactatecalc.js

--- lactatecalc.js
+++ lactatecalc.js
@@ -6,7 +6,6 @@ function toggleTool() {
   
   toolContainer.style.display = isHidden ? 'flex' : 'none';
 
-  // Initialize the graph when the tool is shown for the first time
   if (isHidden && !chart) {
     initializeGraph();
   }
@@ -42,22 +41,13 @@ function initializeGraph() {
     },
     options: {
       responsive: true,
-      maintainAspectRatio: false, // Allow the chart to be flexible
-      aspectRatio: 2, // Set a custom aspect ratio (height/width)
+      maintainAspectRatio: false,
       plugins: {
         legend: { display: false }
       },
       scales: {
-        x: { 
-          title: { display: true, text: 'Load', font: { size: 16 } }, 
-          min: 0,
-          ticks: { font: { size: 14 } } // Adjust font size for x-axis ticks
-        },
-        y: { 
-          title: { display: true, text: 'Lactate Concentration', font: { size: 16 } },
-          min: 0,
-          ticks: { font: { size: 14 } } // Adjust font size for y-axis ticks
-        }
+        x: { title: { display: true, text: 'Load' }, min: 0 },
+        y: { title: { display: true, text: 'Lactate Concentration' }, min: 0 }
       }
     }
   });
